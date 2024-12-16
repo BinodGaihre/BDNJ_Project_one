c4214b8b6791903883c7a1b5b5c3496b1175a9b8
deysipaniagua7 <deysi.paniagua7@gmail.com>
2024-12-16 14:33:25 -0600

    Added Deysi's final files

---
+++ Deysi_text_FINAL.r
@@ -0,0 +1,31 @@
+# Manipulated and analyzed data to show total movie releases and popularity per month.
+    # a. Total Number of Movies Released per Month:
+        # January: 252
+        # February: 107
+        # March: 110
+        # April: 104
+        # May: 115
+        # June: 132
+        # July: 138
+        # August: 112
+        # September: 151
+        # October: 145
+        # November: 155
+        # December: 149
+
+    # b. Sorted data into top 3 and bottom 3 months:
+        # Top 3 Months with the Most Movies Released: January (252), November (155), and September (151)
+        # Bottom 3 Months with the Least Movies Released: April (104), February (107), March (110)
+        # Greatest Difference in Total Movies Released: January to February (145)
+
+    # c. Bar chart and area chart to show the change in total number of movies released per month
+
+    # d. Bar chart to show the most popular movie released per month:
+        # top 3 Most Popular Movies per Month:
+            # May: Interstellar (724.25)
+            # September: Deadpool (514.57)
+            # October: Whiplash (192.53)
+        # Bottom 3 Least Popular Movies per Month: 
+            # November: Fifty Shade of Grey (98.76)
+            # December: Cinderella (101.19)
+            # February: Men in Black (104.12)
